f9f7b82d4e8a059d7d83a138e6be5fd2e47f09c0
Rehan Ahmad <rehan.inboxme@gmail.com>
2025-07-07 11:42:49 -0500

    Update sqlnotes.sql

--- sqlnotes.sql
+++ sqlnotes.sql
@@ -1,5 +1,12 @@
 /*
+SELECT @@servername as DBInstance,db_name() as DBName
+-- use zzrehan
+SELECT name FROM sys.tables
+--EXEC sp_tables @table_type = "'TABLE'";
 
+SELECT name, database_id, create_date
+FROM sys.databases;
+GO
 
 CREATE TABLE CUSTOMERS (
 	ID INT				NOT NULL,
@@ -201,3 +208,222 @@ CREATE PROCEDURE SelectCustomerstabledata
 AS 
 SELECT * FROM Testdb.Customers 
 GO 
+
+--T-SQL - Transactions:  COMMIT − To save the changes, ROLLBACK − To roll back the changes, SAVEPOINT − Creates points within groups of transactions in which to ROLLBACK, SET TRANSACTION − Places a name on a transaction.
+
+-- COMMIT
+
+/*
+Begin Tran 
+DELETE FROM CUSTOMERS 
+   WHERE AGE = 25 
+COMMIT 
+*/
+
+-- ROLLBACK
+/*
+Begin Tran 
+DELETE FROM CUSTOMERS 
+   WHERE AGE = 25; 
+ROLLBACK
+*/
+
+/*
+Begin Tran 
+SAVE Transaction SP1 
+Savepoint created. 
+DELETE FROM CUSTOMERS WHERE ID = 1  
+1 row deleted. 
+SAVE Transaction SP2 
+Savepoint created. 
+DELETE FROM CUSTOMERS WHERE ID = 2 
+1 row deleted.
+SAVE Transaction SP3 
+Savepoint created. 
+DELETE FROM CUSTOMERS WHERE ID = 3 
+1 row deleted.
+*/
+
+/*
+ROLLBACK Transaction SP2 
+Rollback complete. 
+*/
+
+-- SET TRANSACTION ISOLATION LEVEL <Isolationlevel_name>
+
+/* T_SQL Indexes
+CREATE UNIQUE INDEX uniqueindex 
+on customers (NAME)
+
+CREATE INDEX compositeindex 
+on customers (NAME, ID)
+
+DROP INDEX tablename.index_name
+
+*/
+
+/*
+
+
+    SQL Server COUNT Function − The SQL Server COUNT aggregate function is used to count the number of rows in a database table.
+
+    SQL Server MAX Function − The SQL Server MAX aggregate function allows to select the highest (maximum) value for a certain column.
+
+
+
+    SQL Server MIN Function − The SQL Server MIN aggregate function allows to select the lowest (minimum) value for a certain column.
+
+    SQL Server AVG Function − The SQL Server AVG aggregate function selects the average value for certain table column.
+
+    SQL Server SUM Function − The SQL Server SUM aggregate function allows selecting the total for a numeric column.
+
+    SQL Server SQRT Function − This is used to generate a square root of a given number.
+
+    SQL Server RAND Function − This is used to generate a random number using SQL command.
+
+    SQL Server CONCAT Function − This is used to concatenate multiple parameters to a single parameter.
+
+    SQL Server Numeric Functions − Complete list of SQL functions required to manipulate numbers in SQL.
+
+    SQL Server String Functions − Complete list of SQL functions required to manipulate strings in SQL.
+
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages) VALUES (1, 'John', '2007-01-24', 250);
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages) VALUES (2, 'Ram', '2007-05-27', 220);
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages) VALUES (3, 'Jack', '2007-05-06', 170);
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages) VALUES (3, 'Jack', '2007-04-06', 100);
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages) VALUES (4, 'Jill', '2007-04-06', 220);
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages) VALUES (5, 'Zara', '2007-06-06', 300);
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages) VALUES (5, 'Zara', '2007-02-06', 350);
+
+CREATE TABLE employee_tbl (
+    id INT,
+    name VARCHAR(50),
+    work_date DATE,
+    daily_typing_pages INT
+);
+
+	
+	SELECT * FROM employee_tbl;
++------+------+------------+--------------------+
+| id   | name | work_date  | daily_typing_pages |
++------+------+------------+--------------------+
+|    1 | John | 2007-01-24 |                250 |
+|    2 | Ram  | 2007-05-27 |                220 |
+|    3 | Jack | 2007-05-06 |                170 |
+|    3 | Jack | 2007-04-06 |                100 |
+|    4 | Jill | 2007-04-06 |                220 |
+|    5 | Zara | 2007-06-06 |                300 |
+|    5 | Zara | 2007-02-06 |                350 |
++------+------+------------+--------------------+
+7 rows in set (0.00 sec)
+	
+
+	Examples :
+	SELECT COUNT(*) FROM employee_tbl
+   -> WHERE name="Zara";
+
+SELECT MAX(SALARY) FROM CUSTOMERS;
+
+SELECT ID, NAME, SALARY 
+FROM CUSTOMERS 
+GROUP BY NAME, ID 
+HAVING MAX(SALARY) < 8000;
+
+SELECT * FROM CUSTOMERS 
+WHERE SALARY = (SELECT MAX(SALARY) FROM CUSTOMERS);
+
+SELECT MAX(NAME) AS max_name FROM CUSTOMERS;
+SELECT MAX(age) AS 'max_age' FROM CUSTOMERS;
+
+SELECT MIN(SALARY) FROM CUSTOMERS;
+
+SELECT ID, NAME, SALARY 
+FROM CUSTOMERS 
+GROUP BY NAME, ID 
+HAVING MIN(SALARY) > 5000;
+
+SELECT * FROM CUSTOMERS 
+WHERE SALARY = (SELECT MIN(SALARY) FROM CUSTOMERS);
+
+SELECT MIN(NAME) AS min_first_name FROM CUSTOMERS;
+
+SELECT MIN(age) AS 'min_age' FROM CUSTOMERS;
+
+   
+   SELECT AVG(daily_typing_pages)
+    -> FROM employee_tbl;
+	
+	SELECT name, AVG(daily_typing_pages)
+   -> FROM employee_tbl GROUP BY name;
++------+-------------------------+
+| name | AVG(daily_typing_pages) |
++------+-------------------------+
+| Jack |                135.0000 |
+| Jill |                220.0000 |
+| John |                250.0000 |
+| Ram  |                220.0000 |
+| Zara |                325.0000 |
++------+-------------------------+
+
+SELECT SUM(daily_typing_pages)
+   -> FROM employee_tbl;
+
+SELECT name, SUM(daily_typing_pages)
+   -> FROM employee_tbl GROUP BY name;
+
+select SQRT(16);
+SELECT * FROM employee_tbl;
+SELECT name, SQRT(daily_typing_pages)
+   -> FROM employee_tbl;
+   
+
+
+SELECT RAND( ), RAND( ), RAND( );
+SELECT * FROM employee_tbl;  -- repeat
+   
+SELECT CONCAT('Revathi');
+SELECT CONCAT('Tutorials', 'point');
+SELECT CONCAT('Structured', 'Query', 'Language') AS CONCAT_Function;
+SELECT CONCAT('10001011', '10000000') AS CONCAT_function;
+SELECT CONCAT('Good', 'Morning') AS CONCAT_Function;
+SELECT CONCAT('Rollno', '110101') AS CONCAT_Function;
+SELECT CONCAT('62001', '12345', '14203') AS CONCAT_Function;
+SELECT CONCAT(' ');
+SELECT CONCAT('ALL', 'IS', 'WELL', NULL) AS CONCAT_Function; -- result will be NULL
+
+create table CUSTOMERS(
+   ID INT NOT NULL, 
+   NAME VARCHAR(15) NOT NULL, 
+   AGE INT NOT NULL, 
+   ADDRESS CHAR(25), 
+   SALARY DECIMAL(10, 4), PRIMARY KEY(ID)
+);
+
+insert INTO CUSTOMERS(ID, NAME, AGE, ADDRESS, SALARY) VALUES(1, 'Ramesh', 32, 'Ahmedabad', 2000.00);
+insert INTO CUSTOMERS(ID, NAME, AGE, ADDRESS, SALARY) VALUES(2, 'Khilan', 25, 'Delhi', 1500.00);
+insert INTO CUSTOMERS(ID, NAME, AGE, ADDRESS, SALARY) VALUES(3, 'kaushik', 23, 'Kota', 2000.00);
+insert INTO CUSTOMERS(ID, NAME, AGE, ADDRESS, SALARY) VALUES(4, 'Chaitali', 25, 'Mumbai', 6500.00);
+insert INTO CUSTOMERS(ID, NAME, AGE, ADDRESS, SALARY) VALUES(5, 'Hardik', 27, 'Bhopal', 8500.00);
+insert INTO CUSTOMERS(ID, NAME, AGE, ADDRESS, SALARY) VALUES(6, 'Komal', 22, 'MP', 4500.00);
+insert INTO CUSTOMERS(ID, NAME, AGE, ADDRESS, SALARY) VALUES(7, 'Muffy', 24, 'Indore', 10000.00);
+
+SELECT ID, NAME, CONCAT(ID, NAME) AS CONCAT_Function FROM CUSTOMERS;
+
++----+----------+-----------------+
+| ID | NAME     | CONCAT_Function |
++----+----------+-----------------+
+|  1 | Ramesh   | 1Ramesh         |
+|  2 | Khilan   | 2Khilan         |
+|  3 | kaushik  | 3kaushik        |
+|  4 | Chaitali | 4Chaitali       |
+|  5 | Hardik   | 5Hardik         |
+|  6 | Komal    | 6Komal          |
+|  7 | Muffy    | 7Muffy          |
++----+----------+-----------------+
+
+
+SELECT ID, NAME, AGE, ADDRESS, SALARY, CONCAT(ID, NAME, AGE, ADDRESS, SALARY) AS CONCAT_Function FROM CUSTOMERS;
+
+
+ */
+
